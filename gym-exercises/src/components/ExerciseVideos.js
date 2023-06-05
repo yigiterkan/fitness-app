737f1fef49dbb4cf81339a834ab4c57a2bc4a7f1
Yiğit Cumhur Erkan <79741387+yigiterkan@users.noreply.github.com>
2023-06-05 10:16:07 +0300

    First commit

---
+++ gym-exercises/src/components/ExerciseVideos.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Box,Stack,Typography } from '@mui/material'
+
+const ExerciseVideos = ({exerciseVideos, name}) => {
+
+  if(!exerciseVideos.length) return 'loading...'
+
+  return (
+   <Box sx={{ marginTop:{lg:'2oopx',xs:'20px'}}} p="20px">
+    <Typography variant="h3">
+        Watch <span style={{color:'#ff2625', textTransform:"capitalize"}}>{name} </span> exercise videos
+    </Typography>
+    <Stack justifyContent="flex-start"
+    flexWrap="wrap"
+    sx={{
+      flexDirection:{lg:'row'},
+      gap:{lg:'110px', xs:'0'}
+    }}>
+      {exerciseVideos?.slice(0,3).map((item,index)=>(
+        <a 
+          key={index}
+          className='exercise-video'
+          href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+          <Box>
+            <Typography>
+             {item.video.title} 
+            </Typography>
+          </Box>
+
+        </a>
+      ))}
+
+    </Stack>
+
+   </Box>
+  )
+}
+
+export default ExerciseVideos
